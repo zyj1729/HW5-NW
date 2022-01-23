5a72fe091364961de727f8e7d8375c183758e3b9
Parker Grosjean <parker.grosjean@gmail.com>
2022-01-22 20:25:03 -0800

    Initial commit with project outline.

---
+++ align/align.py
@@ -0,0 +1,162 @@
+# Importing Dependencies
+import numpy as np
+from typing import Tuple
+
+# Defining class for Needleman-Wunsch Algorithm for Global pairwise alignment
+class NeedlemanWunsch:
+    """ Class for NeedlemanWunsch Alignment
+
+    Parameters:
+        sub_matrix_file: str
+            Path/filename of substitution matrix
+        gap_open: float
+            Gap opening penalty
+        gap_extend: float
+            Gap extension penalty
+
+    Attributes:
+        seqA_align: str
+            seqA alignment
+        seqB_align: str
+            seqB alignment
+        alignment_score: float
+            Score of alignment from algorithm
+        gap_open: float
+        	Gap opening penalty
+        gap_extend: float
+        	Gap extension penalty
+    """
+    def __init__(self, sub_matrix_file: str, gap_open: float, gap_extend: float):
+        # Init alignment and gap matrices
+        self._align_matrix = None
+        self._gapA_matrix = None
+        self._gapB_matrix = None
+
+        # Init matrices for backtrace procedure
+        self._back = None
+        self._back_A = None
+        self._back_B = None
+
+        # Init alignment_score
+        self.alignment_score = 0
+
+        # Defining amino acid residue alphabet
+        self._alphabet = []
+
+        # Init empty alignment attributes
+        self.seqA_align = ""
+        self.seqB_align = ""
+
+        # Setting gap open and gap extension penalties
+        self.gap_open = gap_open
+        self.gap_extend = gap_extend
+
+        # Generating substitution matrix
+        self.sub_dict = self._read_sub_matrix(sub_matrix_file) # substitution dictionary
+
+    def _read_sub_matrix(self, sub_matrix_file):
+        """
+        DO NOT MODIFY THIS FUNCTION! IT IS ALREADY COMPLETE!
+
+        This function reads in a scoring matrix from any matrix like file.
+        Where there is a line of the residues followed by substitution matrix.
+        This file also saves the alphabet list attribute.
+
+        Parameters:
+            sub_matrix_file: str
+                Name (and associated path if not in current working directory)
+                of the matrix file that contains the scoring matrix.
+
+        Returns:
+            dict_sub: dict
+                Substitution matrix dictionary with tuple of the two residues as
+                the key and score as value e.g. {('A', 'A'): 4} or {('A', 'D'): -8}
+        """
+        with open(sub_matrix_file, 'r') as f:
+            dict_sub = {}  # Dictionary for storing scores from sub matrix
+            residue_list = []  # For storing residue list
+            start = False  # trigger for reading in score values
+            res_2 = 0  # used for generating substitution matrix
+            # reading file line by line
+            for line_num, line in enumerate(f):
+                # Reading in residue list
+                if '#' not in line.strip() and start is False:
+                    residue_list = [k for k in line.strip().upper().split(' ') if k != '']
+                    start = True
+                # Generating substitution scoring dictionary
+                elif start is True and res_2 < len(residue_list):
+                    line = [k for k in line.strip().split(' ') if k != '']
+                    # reading in line by line to create substitution dictionary
+                    assert len(residue_list) == len(line), "Score line should be same length as residue list"
+                    for res_1 in range(len(line)):
+                        dict_sub[(residue_list[res_1], residue_list[res_2])] = float(line[res_1])
+                    res_2 += 1
+                elif start is True and res_2 == len(residue_list):
+                    break
+        # saving the residue alphabet for use in downstream sequence alignment
+        self._alphabet = residue_list
+        return dict_sub
+
+    def align(self, seqA: str, seqB: str) -> float:
+        """
+        # TODO: Fill in the Needleman-Wunsch Algorithm below
+        to perform global sequence alignment of seqA and seqB.
+        Also, write up a docstring for this function using the
+        _read_sub_matrix as an example.
+        Don't forget to comment your code!
+        """
+        # Initialize 6 matrix private attributes for use in alignment
+		# create matrices for alignment scores and gaps
+		self._align_matrix = np.ones((len(seqA) + 1, len(seqB) + 1)) * -np.inf
+		self._gapA_matrix = np.ones((len(seqA) + 1, len(seqB) + 1)) * -np.inf
+		self._gapB_matrix = np.ones((len(seqA) + 1, len(seqB) + 1)) * -np.inf
+
+		# create matrices for pointers used in backtrace procedure
+		self._back = np.ones((len(seqA) + 1, len(seqB) + 1)) * -np.inf
+		self._back_A = np.ones((len(seqA) + 1, len(seqB) + 1)) * -np.inf
+		self._back_B = np.ones((len(seqA) + 1, len(seqB) + 1)) * -np.inf
+
+		# TODO Implement the global sequence alignment below
+		pass
+
+		self.alignment_score, self.seqA_align, self.seqB_align = self._backtrace()
+		alignment_score = self.alignment_score
+		return alignment_score
+
+	def _backtrace(self) -> Tuple[float, str, str]:
+		"""
+		# TODO Implement the traceback procedure method below
+		based on the heuristic you implement in the align method.
+		The traceback method should return a tuple of the alignment
+		score, the seqA alignment and the seqB alignment respectively.
+		"""
+		# Implement this method based upon the heuristic chosen in the align method above.
+		pass
+
+
+def read_fasta(fasta_file: str) -> Tuple[str, str]:
+    """
+    This function reads in a FASTA file and returns the associated
+    string of characters (residues or nucleotides) and the header.
+
+    Parameters:
+        fasta_file: str
+            name (and associated path if not in current working directory)
+            of the Fasta file.
+
+    Returns:
+        seq: str
+            String of characters from FASTA file
+        header: str
+            Fasta header
+    """
+    with open(fasta_file) as f:
+        is_header = True
+        seq = ''  # initializing sequence
+        for line in f:
+            if is_header and line.strip().startswith('>'):
+                header = line.strip()  # reading in fasta header
+                is_header = False
+            else:
+                seq += line.strip().upper()  # generating full seq
+    return seq, header
---
+++ main.py
@@ -0,0 +1,18 @@
+# Import NeedlemanWunsch class and read_fasta function
+from align import read_fasta, NeedlemanWunsch
+
+def main():
+    """
+    This function should read in all of the species
+    """
+    hs_seq, hs_header = read_fasta("./data/Homo_sapiens_BRD2.fa")
+    gg_seq, gg_header = read_fasta("./data/Gallus_gallus_BRD2.fa")
+    mm_seq, mm_header = read_fasta("./data/Mus_musculus_BRD2.fa")
+    br_seq, br_header = read_fasta("./data/Balaeniceps_rex_BRD2.fa")
+    tt_seq, tt_header = read_fasta("./data/tursiops_truncatus_BRD2.fa")
+
+    # TODO Align all species to humans and print species in order of most similar to human BRD
+    pass
+
+if __name__ == "__main__":
+    main()
